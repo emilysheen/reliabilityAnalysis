0a7b7bfa3cb11f258f1c599e5beba7e49d28af4f
emilysheen <esheen15@gmail.com>
2023-05-18 00:30:42 -0600

    MCF plots and data adjustments

--- 1 create data.py
+++ 1 create data.py
@@ -2,7 +2,7 @@
 ###              Reliability Analysis                 ###
 ###                   Emily Sheen                     ###
 ###        1) Create automobile contract data         ###
-###        2) Create repair failures data               ###
+###        2) Create repair failures data             ###
 #########################################################
 
 ### Assume we have a sample of 10,000 vehicles from model years 2019, 2020, and 2021 with 36 month/36,000 mile bumper to bumper NVLW's.
@@ -71,20 +71,6 @@ print(cars.groupby('car_type').agg(
 print(car_type_mileage.sort_values(by='car_type'))
 # Looks good
 
-
-
-########################################################################################################################
-######  2) REPAIR Failures DATA:  Let's assume we were approached about problematic engine block failures leading  #####
-######     to extensive NVLW claims.  We are interested in discovering the root cause of the engine block problems,#####
-######     which vehicle types and model years are afflicted with the issue, and whether a recall is necessary.    #####
-######  *     Since for this example, we are designing the dataset to be explored, we will assume this is a 2020   #####
-######     and 2021 problem for trucks and heavy duty trucks.  We will assume Poisson distribution with lambda = 3 #####
-######     for 2020 and 2021 trucks and heavy duty trucks.  Let lambda values vary for other vehicle types and     #####
-######     model years, between 0.01 and 0.5.                                                                      #####
-######  *     Let lambda = 3 for Engine Block claims w/in the 3 yr / 36K mi NVLW period, both trucks MY 2020/21    #####
-######  *     Let lambda < 1 for all other car types, all 3 model years                                            #####
-########################################################################################################################
-
 # Let's generate a random number of engine block failures for each vehicle in our sample
 # Set lambda values for each car type and model year
 
@@ -131,6 +117,19 @@ cars['nvlw_end_date'] = cars[['nvlw_mileout','nvlw_timeout']].min(axis=1)
 
 cars.to_csv("cars_data.csv", header=True, index=False)
 
+########################################################################################################################
+######  2) REPAIR Failures DATA:  Let's assume we were approached about problematic engine block failures leading  #####
+######     to extensive NVLW claims.  We are interested in discovering the root cause of the engine block problems,#####
+######     which vehicle types and model years are afflicted with the issue, and whether a recall is necessary.    #####
+######  *     Since for this example, we are designing the dataset to be explored, we will assume this is a 2020   #####
+######     and 2021 problem for trucks and heavy duty trucks.  We will assume Poisson distribution with lambda = 3 #####
+######     for 2020 and 2021 trucks and heavy duty trucks.  Let lambda values vary for other vehicle types and     #####
+######     model years, between 0.01 and 0.5.                                                                      #####
+######  *     Let lambda = 3 for Engine Block claims w/in the 3 yr / 36K mi NVLW period, both trucks MY 2020/21    #####
+######  *     Let lambda < 1 for all other car types, all 3 model years                                            #####
+########################################################################################################################
+
+
 #### Now we need to determine which day in the NVLW period the failures will occur.
 
 fails = cars[["vin", "purchase_date", 'nvlw_end_date', "n_fails"]]
@@ -148,37 +147,36 @@ print(fails.dtypes)
 fails.loc[fails['nvlw_end_date'] >= pd.to_datetime(datetime.date.today(), format='%Y-%m-%d'), 'censor_date'].unique()
 
 fails[['censor_date', 'purchase_date']] = fails[['censor_date', 'purchase_date']].apply(pd.to_datetime)
-fails['days_to_censor'] = (fails['censor_date'] - fails['purchase_date']).dt.days
+fails['days_purchase_to_censor'] = (fails['censor_date'] - fails['purchase_date']).dt.days
 
 # If the vehicle has 0 claims, we just need the row for censor date
 # If a vehicle has 1+ claim, we need a row for each claim and the censor date
 # Let's split up the censor / claim data to simplify transformation
-fails = fails.loc[fails.index.repeat(fails.n_fails + 1)]  # duplicates rows for n_fails + 1
+fails = fails.loc[fails.index.repeat(fails.n_fails + 1)]  # duplicates rows for n_fails + 1, sorts by vin and purchase_date
+fails = fails.sort_values(by=['purchase_date', 'vin'], axis=0)
 
 censors = fails[fails.duplicated()==False] # 1st copy of each vehicle to get censor date
 len(censors)
 fails = fails[fails.duplicated()]  # duplicate copies for claims == claims[claims['n_fails'] >= 1]
 
 # For the censor data, just set the date to the censor date from earlier and the status to Censor
-censors['days_to_censor_fail'] = censors['days_to_censor']
+censors['days_purchase_to_censor_fail'] = censors['days_purchase_to_censor']
 censors['censor_fail_status'] = 'C'
 censors['censor_fail_date'] = censors['censor_date']
 
 # We need to generate random failure dates, we will assume uniform distribution for simplicity
-
-# claims['days_to_censor_fail'] = [random.randint(1, 1094) for i in range(0, len(claims))]
 fails['censor_fail_status'] = 'F'
 
-# Check for small windows in [purchase_date, censor_date] and negative days_to_censor values
-fails.loc[fails['days_to_censor'] < 0]
+# Check for small windows in [purchase_date, censor_date] and negative days_purchase_to_censor values
+len(fails.loc[fails['days_purchase_to_censor'] < 0]) == 0
 
 pd.set_option('display.max_columns', None)
-print(cars.loc[cars['nvlw_end_date'] < cars['purchase_date']])
+len(cars.loc[cars['nvlw_end_date'] < cars['purchase_date']]) == 0
 
 def add_claim_days(row):
-    high_lim = row['days_to_censor'] - 1
+    high_lim = row['days_purchase_to_censor'] - 1
     add_days = random.randint(1, high_lim)
-    row['days_to_censor_fail'] = add_days
+    row['days_purchase_to_censor_fail'] = add_days
     end_date = row['purchase_date'] + datetime.timedelta(days=add_days)
     end_date = pd.to_datetime(end_date, format='%Y-%m-%d')
     row['censor_fail_date'] = end_date
@@ -186,6 +184,7 @@ def add_claim_days(row):
 
 fails = fails.apply(lambda row: add_claim_days(row), axis=1)
 
+len(fails.loc[fails['censor_fail_date'] > fails['censor_date']]) == 0
 fails = fails.loc[fails['censor_fail_date'] <= fails['censor_date']]
 len(fails)
 
@@ -193,7 +192,41 @@ new_n_fails = fails.groupby('vin')['censor_fail_status'].apply(lambda x: (x=='F'
 max(new_n_fails['count'])
 sum(new_n_fails['count'])
 # Merge back failures and censors
-fails_censors = pd.concat([fails, censors]).sort_values(by=['purchase_date', 'vin', 'censor_fail_date'], axis=0)
+fails_censors = pd.concat([fails, censors]).sort_values(by=['purchase_date', 'vin', 'censor_fail_date'], axis=0).reset_index(drop=True)
+fails_censors.head(100)
+
+# Now, for each event we need the days since prior event.
+# For 1st failure, days_since_last = days from purchase date to 1st fail
+# For 2nd - last failure, days_since_last = days from (i-1)th failure to this ith failure
+# For censoring, days_since_last = days from purchase date to censor date for cars with no failures
+#       and days_since_last = days from last failure to censor date for claims with failures.
+
+# using a for loop for simplicity, but probably a better way to count events
+# for each row, 1) check if vin is the same as the last row
+#               2) if vin is the same as last row, let vin_event_number = last_row_vin_event_number + 1
+#                   2a) if this_vin == last_vin & vin_event_number == 1 (after updating it):
+#                       days_since_event is days from purchase_date to this row's fail_censor_date
+#                       row['days_since_event'] = row['days_purchase_to_censor_fail']
+#               3) if vin changed from last row, assign vin_event_number = 0
+
+fails_censors['vin_event_number'] = None
+fails_censors['days_since_event'] = None
+last_vin = None
+vin_event_number = 1
+last_days = 0
+for index, row in fails_censors.sort_values(by=['purchase_date', 'vin', 'censor_fail_date'], axis=0).iterrows():
+    this_vin = row['vin']
+    this_days = row['days_purchase_to_censor_fail']
+    if this_vin == last_vin:
+        vin_event_number = vin_event_number+1
+        fails_censors.at[index, 'days_since_event'] = this_days - last_days  # subtract prior events days since purchase_date
+    else: # this row new vin
+        vin_event_number = 1
+        fails_censors.at[index, 'days_since_event'] = this_days
+    fails_censors.at[index, 'vin_event_number'] = vin_event_number
+    last_vin = this_vin
+    last_days = this_days
+
 fails_censors.head(100)
 
 fails_censors.to_csv("failures_censors_data.csv", header=True, index=False)
--- 2 Failure Rate Analysis.py
+++ 2 Failure Rate Analysis.py
@@ -14,15 +14,16 @@ import numpy as np
 from reliability.Nonparametric import KaplanMeier
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('failures_censors_data.csv').sort_values(by=['vin', 'days_to_censor_fail'])
+df = pd.read_csv('failures_censors_data.csv').sort_values(by=['purchase_date', 'vin', 'days_purchase_to_censor_fail']).reset_index(drop=True)
+cars = pd.read_csv('cars_data.csv').sort_values(by=['purchase_date', 'vin']).reset_index(drop=True)
 # pd.set_option('display.max_columns', None)
 first_fails = df[df.duplicated(['vin', 'censor_fail_status'])==False]
 len(first_fails.loc[first_fails['censor_fail_status'] == 'C'])
 check = pd.DataFrame({'number_events' : first_fails.groupby( ['vin', 'censor_fail_status'] ).size()}).reset_index()
 max(check['number_events'])
 
-fails = first_fails.loc[first_fails['censor_fail_status'] == 'F', 'days_to_censor_fail'].to_numpy()
-censors = first_fails.loc[first_fails['censor_fail_status'] == 'C', 'days_to_censor_fail'].to_numpy()
+fails = first_fails.loc[first_fails['censor_fail_status'] == 'F', 'days_purchase_to_censor_fail'].to_numpy()
+censors = first_fails.loc[first_fails['censor_fail_status'] == 'C', 'days_purchase_to_censor_fail'].to_numpy()
 
 # FIRST WE WILL EXAMINE THE SURVIVAL FUNCTIONS, ONLY CONSIDERING THE FIRST FAILURES
 # This gives us an idea of the percentage of units experiencing failures by Day since purchase_date
@@ -41,6 +42,7 @@ plt.xlabel('Days to Failure')
 plt.legend()
 plt.show()
 plt.savefig("plots/KM and Weibull Survival Functions - No Censoring.jpg")
+plt.close()
 
 plt.figure(figsize=(12, 5))
 plt.suptitle("Survival Functions for First Failures\nWith Censoring", fontsize=16)
@@ -55,7 +57,7 @@ plt.xlabel('Days to Failure')
 plt.legend()
 plt.show()
 plt.savefig("plots/KM and Weibull Survival Functions - With Censoring.jpg")
-
+plt.close()
 
 '''
 IGNORING RIGHT CENSORING, THE WEIBULL FIT IS AS FOLLOWS:
@@ -96,7 +98,14 @@ Goodness of fit    Value
 ###           78               F                 1                                    ###
 ###           90               C                 4                                    ###
 ###          103               C                 2                                    ###
-###   * This format simplifies to the case where number_events == 1 for each row.     ###
+###  * This format simplifies to the case where number_events == 1 for each row.      ###
+###                                                                                   ###
+###  * MULTIPLE EVENTS PER VEHICLE: Since we have multiple events per vehicle in      ###
+###    some cases, we will assume the repair/replacement fixed the issue on the       ###
+###    same day that the failure happened, for simplicity.  Generally repairs could   ###
+###    take days or weeks and we would want to start the clock again once the car     ###
+###    is back in service.  After each repair, we reset the clock so that the next    ###
+###    event (or censoring) is based on days since last repair/replacement.           ###
 ###                                                                                   ###
 ###  * Reliability package allows us to fit several distributions to our sample       ###
 ###    ~ Functions to fit Non-Location-Shifted Distributions: Fit_Exponential_1P,     ###
@@ -107,13 +116,14 @@ Goodness of fit    Value
 #########################################################################################
 df.head
 
-df_xc = df[['days_to_censor_fail','censor_fail_status']].copy().rename(
-    columns={'days_to_censor_fail':'event_time', 'censor_fail_status':'censor_code'}).sort_values(
-    by=['event_time'], ascending=True)
-df_xc.groupby(['event_time', 'censor_code']).size()
-df_xcn = pd.DataFrame({'number_events' : df_xc.groupby( ['event_time', 'censor_code'] ).size()}).reset_index()
-df_xc.head
-df_xcn.head
+# Using purchase date rather than resetting clock after repair/replacement
+df_xc_buy = df[['days_purchase_to_censor_fail','censor_fail_status']].copy().rename(
+    columns={'days_purchase_to_censor_fail':'event_time', 'censor_fail_status':'censor_code'}).sort_values(
+    by=['event_time'], ascending=True).reset_index(drop=True)
+df_xc_buy.groupby(['event_time', 'censor_code']).size()
+df_xcn_buy = pd.DataFrame({'number_events' : df_xc_buy.groupby( ['event_time', 'censor_code'] ).size()}).reset_index()
+df_xc_buy.head
+df_xcn_buy.head
 
 from reliability.Distributions import Weibull_Distribution
 from reliability.Fitters import Fit_Weibull_3P
@@ -122,16 +132,17 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KernelDensity
 
 # Let's try again to fit Weibull distribution incorporating the censoring data using df_xc
-fails = (df_xc.loc[df_xc['censor_code'] == 'F', 'event_time']).to_numpy()
-censors = (df_xc.loc[df_xc['censor_code'] == 'C', 'event_time']).to_numpy()
+fails = (df_xc_buy.loc[df_xc_buy['censor_code'] == 'F', 'event_time']).to_numpy()
+censors = (df_xc_buy.loc[df_xc_buy['censor_code'] == 'C', 'event_time']).to_numpy()
 wbf = Fit_Weibull_3P(failures=fails,
                      right_censored=censors,
                      show_probability_plot=True, print_results=True)  # fit the Weibull_3P distribution
 print('Fit_Weibull_3P parameters:\nAlpha:', wbf.alpha, '\nBeta:', wbf.beta, '\nGamma', wbf.gamma)
 # histogram(fails) # generates the histogram of failures, which is uniform until censoring reduces the claims
+plt.close()
 
 # Histogram with kernel density estimate
-dist_kernel = KernelDensity(bandwidth=40, kernel='gaussian')  # bandwidth = 2 is very choppy
+dist_kernel = KernelDensity(bandwidth=30, kernel='exponential')  # bandwidth = 2 is very choppy
 sample = fails.reshape((len(fails), 1))
 dist_kernel.fit(sample)
 # sample probabilities for a range of outcomes
@@ -142,12 +153,50 @@ probabilities = np.exp(probabilities)
 # plot the histogram and pdf
 plt.hist(sample, bins=50, density=True)
 plt.plot(values[:], probabilities, label='Kernel Density Estimate')
+plt.xlabel("Days Since Vehicle Purchase")
 # wbf.distribution.PDF(xmin=0, xmax=1096, label='Fit_Weibull_3P', linestyle='--')  # plots to PDF of the fitted Weibull_3P, not comparable
-plt.title('NVLW Failure Emergence:\nFlat until cars leave NVLW')
+plt.title('NVLW Failure Emergence for Days since Vehicle Purchase')
 plt.legend()
 plt.show()
-plt.savefig("plots/NVLW Failure Emergence.jpg")
+plt.savefig("plots/NVLW Failure Emergence for Days since Vehicle Purchase.jpg")
+plt.close()
 
+# Let's try again to fit Weibull distribution now treating repeat claims as independent events (restarting the clock after repair/replacement)
+df_xc = df[['days_since_event','censor_fail_status']].copy().rename(
+    columns={'days_since_event':'event_time', 'censor_fail_status':'censor_code'}).sort_values(
+    by=['event_time'], ascending=True).reset_index(drop=True)
+df_xc.groupby(['event_time', 'censor_code']).size()
+df_xcn = pd.DataFrame({'number_events' : df_xc.groupby( ['event_time', 'censor_code'] ).size()}).reset_index()
+df_xc.head
+df_xcn.head
+fails = (df_xc.loc[df_xc['censor_code'] == 'F', 'event_time']).to_numpy()
+censors = (df_xc.loc[df_xc['censor_code'] == 'C', 'event_time']).to_numpy()
+wbf = Fit_Weibull_3P(failures=fails,
+                     right_censored=censors,
+                     show_probability_plot=True, print_results=True)  # fit the Weibull_3P distribution
+print('Fit_Weibull_3P parameters:\nAlpha:', wbf.alpha, '\nBeta:', wbf.beta, '\nGamma', wbf.gamma)
+# histogram(fails) # generates the histogram of failures, which is uniform until censoring reduces the claims
+plt.close()
+
+# Histogram with kernel density estimate
+dist_kernel = KernelDensity(bandwidth=30, kernel='exponential')  # bandwidth = 2 is very choppy
+sample = fails.reshape((len(fails), 1))
+dist_kernel.fit(sample)
+# sample probabilities for a range of outcomes
+values = np.asarray([value for value in range(1, 1096)])
+values = values.reshape((len(values), 1))
+probabilities = dist_kernel.score_samples(values)
+probabilities = np.exp(probabilities)
+# plot the histogram and pdf
+plt.hist(sample, bins=50, density=True)
+plt.plot(values[:], probabilities, label='Kernel Density Estimate')
+plt.xlabel("Days Since Last Event")
+# wbf.distribution.PDF(xmin=0, xmax=1096, label='Fit_Weibull_3P', linestyle='--')  # plots to PDF of the fitted Weibull_3P, not comparable
+plt.title('NVLW Failure Emergence for Days since Last Event')
+plt.legend()
+plt.show()
+plt.savefig("plots/NVLW Failure Emergence for Days since Last Event.jpg")
+plt.close()
 
 '''
 * Probability plot is bendy suggesting poor fit.  Makes sense because we did not generate the failures data
@@ -168,22 +217,102 @@ Goodness of fit   Value
              AD  155736 
 '''
 
-
-
-
+#########################################################################################
+###    *  Mean Cumulative Function for Average Cumulative # Failures on Vehicles      ###
+###                                                                                   ###
+###    In order to study multiple failures per vehicle within the NVLW, the           ###
+###      Mean Cumulative Function (MCF) is a nonparametric measurement similar to     ###
+###     the Kaplan Meier survival curve, but it shows average # cum failures over     ###
+###     the vehicles' lifetimes, rather than time until failure (first failure)       ###
+###   DATA FORMAT needs to be a list of lists, with each inner list representing      ###
+###      a single vehicle, the last time being the censor time for that car           ###
+#########################################################################################
 
 ### MCF Example
 from reliability.Repairable_systems import MCF_nonparametric
 from reliability.Datasets import MCF_2
 import matplotlib.pyplot as plt
-times = [[5, 10, 15, 17], [6, 13, 17, 19], [12, 20, 25, 26], [13, 15, 24], [16, 22, 25, 28]]
-MCF_nonparametric(data=times)
-plt.show()
 
+times = []
+for vin in df['vin'].unique():
+    vin_times = (df.loc[df['vin'] == vin, 'days_purchase_to_censor_fail']).tolist()
+    times.append(vin_times)
+
+MCF_nonparametric(data=times)
+plt.title("Non-Parametric MCF for All Vehicles")
+plt.xlabel("Days since NVLW Start")
+plt.show()
+plt.savefig("plots/NonParametric MCF for All Vehicles.jpg")
+plt.close()
 
 from reliability.Repairable_systems import MCF_parametric
 times = MCF_2().times
 MCF_parametric(data=times)
+plt.xlabel("Days since NVLW Start")
 plt.show()
+plt.savefig("plots/Parametric MCF for All Vehicles.jpg")
 
 
+
+#  Now we want to try to figure out WHY we have such high repair rates early in the vehicles' lives
+#  We have a few other synthesized variables we can look at by left joining in our car_data.csv file
+
+fails = pd.read_csv('failures_censors_data.csv').sort_values(by=['purchase_date', 'vin', 'days_purchase_to_censor_fail']).reset_index(drop=True)
+cars = pd.read_csv('cars_data.csv').sort_values(by=['purchase_date', 'vin']).reset_index(drop=True)
+
+dat = cars.merge(fails, on=['vin', 'purchase_date', 'n_fails', 'nvlw_end_date'], how='left').sort_values(
+    by=['purchase_date', 'vin', 'days_purchase_to_censor_fail']).reset_index(drop=True)
+
+# pd.set_option('display.max_columns', None)
+dat.head()
+
+car_types = dat['car_type'].unique()
+model_years = dat['model_year'].unique()
+
+# for cart in car_types:
+#     for my in model_years:
+#         globals()['time_%s' % cart + str(my)] = 'Hello'
+
+import re
+regex = re.compile('[^a-zA-Z]')
+#First parameter is the replacement, second parameter is your input string
+# regex.sub('', 'ab3d*E')
+#Out: 'abdE'
+
+for cart in car_types:
+    for my in model_years:
+        cart_name = regex.sub('', cart)
+        ldat = dat.loc[(dat['model_year'] == my) & (dat['car_type'] == cart), ['vin', 'days_purchase_to_censor_fail']].sort_values(
+            by=['vin', 'days_purchase_to_censor_fail']).reset_index(drop=True)
+        ltimes = []
+        for vin in ldat['vin'].unique():
+            vin_times = (df.loc[df['vin'] == vin, 'days_purchase_to_censor_fail']).tolist()
+            ltimes.append(vin_times)
+        globals()['times_%s' % str(my) + cart] = ltimes
+        # Plot and save both MCF's
+        plt.figure(figsize=(12, 5))
+        plt.suptitle("MCFs for " + str(my) + " " + cart, fontsize=16)
+        plt.subplot(121)
+        MCF_nonparametric(data=ltimes)
+        plt.xlabel('Days since NVLW Start')
+        plt.title('Non-Parametric MCF')
+        plt.subplot(122)
+        MCF_parametric(data=ltimes)
+        plt.title("Parametric MCF")
+        plt.xlabel("Days since NVLW Start")
+        plt.show()
+        plt.savefig("plots/MCFs by Vehicle and MY/MCF for " + str(my) + " "+ cart + ".jpg")
+        plt.close()
+
+
+# After splitting up the MCF's for each model year and vehicle type, we see that Trucks and Heavy Duty Trucks have
+# the largest rates of failure during the 3 year limited warranty.
+#  * 2019 TRUCKS reach 0.4 mean cumulative repairs by the end of the 3 year limited warranty
+#  * 2019 HEAVY DUTY TRUCKS reach 0.8 mean cumulative failures per truck before the end of the NVLW
+#  * 2020 TRUCKS reach ALMOST 5 mean cumulative repairs by the end of the 3 year limited warranty
+#  * 2020 HEAVY DUTY TRUCKS reach ALMOST 5 mean cumulative failures per truck before the end of the NVLW
+#  * 2021 TRUCKS reach 4 mean cumulative failures per truck before the end of the NVLW
+#  * 2021 HEAVY DUTY TRUCKS reach OVER 5 mean cumulative failures per truck before the end of the NVLW
+
+# FROM THESE MCF PLOTS, we see that the problem starts to pick up in 2020 model year trucks, and persists in the 2021 model year
+# We also notice visually that the parametric
